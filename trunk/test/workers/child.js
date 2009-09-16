ab20a8c3829c42a6c1a6553d9c4f95b8505b118b
jonathan.j5.cook <jonathan.j5.cook@cf2c6c28-9e1e-11de-81d6-937ba7f528c2>
2009-09-16 22:39:15 +0000

    updated structure of tests, now split into three suites

--- trunk/test/workers/child.js
+++ trunk/test/workers/child.js
@@ -3,6 +3,8 @@ var maxNest = "worker-child-child-child";
 
 function buildOnMessageHandler(callback) {
 	return function(event) {
+		postMessage(testingRole + " received: " + event.data);
+		
 		if (event.data.indexOf && event.data.indexOf("testing")===0) {
 			postMessage(event.data.replace("testing","tested"));
 		} else if (event.data.indexOf && event.data.indexOf("tested")===0) {
@@ -16,8 +18,17 @@ function buildOnMessageHandler(callback) {
 onmessage = function (event) {
 	testingRole = event.data;
 	// post the results of running tests back to the worker
-	var results = jsUnity.run(dWT);
-	postMessage(results);
+	onmessage = function (event) {
+		postMessage(testingRole + " received: " + event.data);
+	};
+	
+	postMessage(jsUnity.run(dWCT));
+	setTimeout(function () {
+		postMessage(jsUnity.run(dWPT));
+	},500);
+	setTimeout(function () {
+		postMessage(jsUnity.run(dWWT));
+	},1000)
 };
 
 /* Load Test Suite */
@@ -30,10 +41,12 @@ jsUnity.log = function (msg) {
 jsUnity.error = function(msg) {
 	postMessage("JSUNITY: ERROR: " + msg);
 };
-var dWT = jsUnity.compile(dedicatedWorkerTests);
-dWT.scope = this;
+var dWCT = jsUnity.compile(dedicatedWorkerCoreTests),
+	dWPT = jsUnity.compile(dedicatedWorkerPostMessageTests),
+	dWWT = jsUnity.compile(dedicatedWorkerWorkerTests);
+dWCT.scope = dWPT.scope = dWWT.scope = this;
 
-function dedicatedWorkerTests() {
+function dedicatedWorkerCoreTests() {
 	
 	// test importScripts functionality
 	function testImport () {
@@ -58,28 +71,14 @@ function dedicatedWorkerTests() {
 		assertEqual("undefined",typeof(DedicatedWorkerGlobalScope), "dedicated worker global scope is undefined");
 		assertEqual("undefined",typeof(SharedWorkerGlobalScope), "shared worker global scope is undefined");
 	}
-	
-	function testWorker() {
-		if (testingRole !== maxNest) {
-			assertNotUndefined(Worker,"worker is defined");
-			var w = new Worker("child.js");
-			
-			w.onmessage = buildOnMessageHandler(function (event) {
-				assertEqual("tested " + testingRole + "-child", event.data, "testing " + testingRole + "-child");
-			});
-			
-			// establish the worker's role
-			w.postMessage(testingRole+"-child");
-			
-			// send it a message, triggering the onmessage handler above
-			w.postMessage("testing " + testingRole + "-child");
-		}
-	}
-	
+}
+
+function dedicatedWorkerPostMessageTests() {
+
 	// test post message
 	function testPostMessage() {
 		assertNotUndefined("postMessage is defined",postMessage);
-		
+	
 		// expect identical reply
 		onmessage = buildOnMessageHandler(function (event) {
 			assertEqual("tested postMessage",event.data,"reply is identical to posted message");
@@ -87,5 +86,31 @@ function dedicatedWorkerTests() {
 
 		// send message
 		postMessage("testing postMessage");
+		while (i < 100000) {
+			i++;
+		}
+	}
+}
+
+function dedicatedWorkerWorkerTests() {
+
+	function testWorker() {
+		if (testingRole !== maxNest) {
+			assertNotUndefined(Worker,"worker is defined");
+			var w = new Worker("child.js");
+		
+			w.onmessage = buildOnMessageHandler(function (event) {
+				assertEqual("tested " + testingRole + "-child", event.data, "testing " + testingRole + "-child");
+			});
+		
+			// establish the worker's role
+			w.postMessage(testingRole+"-child");
+		
+			// send it a message, triggering the onmessage handler above
+			w.postMessage("testing " + testingRole + "-child");
+			while (i < 100000) {
+				i++;
+			}
+		}
 	}
 }
