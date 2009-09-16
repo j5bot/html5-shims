cc3abaf7f9f5302b8d756a65230a13546e90f502
jonathan.j5.cook <jonathan.j5.cook@cf2c6c28-9e1e-11de-81d6-937ba7f528c2>
2009-09-16 22:56:49 +0000

    even simpler I hope

--- test/workers/simplechild.js
+++ test/workers/simplechild.js
@@ -1,14 +1,3 @@
 onmessage = function (event) {
 	postMessage("received: " + event.data);
 };
-
-/* Load Test Suite */
-/* importScripts("../jsunity/jsunity.js");
-
-jsUnity.attachAssertions();
-jsUnity.log = function (msg) {
-	postMessage("JSUNITY: " + msg);
-};
-jsUnity.error = function(msg) {
-	postMessage("JSUNITY: ERROR: " + msg);
-}; */
