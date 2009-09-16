5d17bb43a24c3a3cbd93c1be53200527d00b8ab6
jonathan.j5.cook <jonathan.j5.cook@cf2c6c28-9e1e-11de-81d6-937ba7f528c2>
2009-09-16 22:53:10 +0000

    added a new child for testing

---
+++ trunk/test/workers/simplechild.js
@@ -0,0 +1,14 @@
+onmessage = function (event) {
+	postMessage("received: " + event.data);
+};
+
+/* Load Test Suite */
+importScripts("../jsunity/jsunity.js");
+
+jsUnity.attachAssertions();
+jsUnity.log = function (msg) {
+	postMessage("JSUNITY: " + msg);
+};
+jsUnity.error = function(msg) {
+	postMessage("JSUNITY: ERROR: " + msg);
+};
